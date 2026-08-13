ef47d4f9efbbb07d186006492e9a423f36284df9
nkj <209133841+t-nkj@users.noreply.github.com>
2026-08-14 01:27:17 +0900

    exeファイルにアイコンが表示されるように

--- build.rs
+++ build.rs
@@ -8,10 +8,13 @@ fn main() {
             .decode()
             .expect("decode application icon")
             .into_rgba8();
-        let icon =
-            ico::IconImage::from_rgba_data(source.width(), source.height(), source.into_raw());
         let mut directory = ico::IconDir::new(ico::ResourceType::Icon);
-        directory.add_entry(ico::IconDirEntry::encode(&icon).expect("encode application icon"));
+        for size in [16, 24, 32, 48, 64, 128, 256] {
+            let resized =
+                image::imageops::resize(&source, size, size, image::imageops::FilterType::Lanczos3);
+            let icon = ico::IconImage::from_rgba_data(size, size, resized.into_raw());
+            directory.add_entry(ico::IconDirEntry::encode(&icon).expect("encode application icon"));
+        }
         let icon_path =
             std::path::PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR")).join("qshare.ico");
         let file = std::fs::File::create(&icon_path).expect("create ico");
@@ -19,6 +22,8 @@ fn main() {
 
         let mut resource = winresource::WindowsResource::new();
         resource.set_icon(icon_path.to_str().expect("icon path is UTF-8"));
+        resource.set("ProductName", "QShare");
+        resource.set("FileDescription", "QShare for Windows");
         resource.compile().expect("compile Windows resources");
     }
 }
